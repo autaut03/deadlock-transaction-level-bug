f3e09ce8ee3ca7db2f8ee632e7fcd13caca24a6c
Alex Wells <autaut03@gmail.com>
2020-04-26 01:51:19 +0300

    Drop dead code

--- tests/Unit/DecrementsTransactionLevelEvenIfCaughtExceptionTest.php
+++ tests/Unit/DecrementsTransactionLevelEvenIfCaughtExceptionTest.php
@@ -1,12 +1,9 @@
 <?php
 
-
 namespace Tests\Unit;
-
 
 use App\User;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Queue;
 use PDOException;
 use Tests\TestCase;
 use Throwable;
@@ -71,56 +68,5 @@ class DecrementsTransactionLevelEvenIfCaughtExceptionTest extends TestCase
         // will fail as savepoint doesn't exist anymore
         // PDOException : SQLSTATE[42000]: Syntax error or access violation: 1305 SAVEPOINT trans2 does not exist
         DB::rollBack();
-
-        sleep(5);
-
-//    $secondConnection->transaction(function () use ($secondConnection, $secondConnectionName, $user) {
-//        $caughtException = null;
-//
-//        // laravel transaction level = 1, mysql transaction level = 1
-//        $secondConnection->beginTransaction();
-//        $this->assertSame(1, $secondConnection->transactionLevel());
-//
-//        // laravel transaction level = 2, mysql transaction level = 2
-//        $secondConnection->beginTransaction();
-//        $this->assertSame(2, $secondConnection->transactionLevel());
-//
-//        try {
-//            // Cause a deadlock
-//            $firstUser
-//                ->setConnection($secondConnectionName)
-//                ->whereKey($firstUser->getKey())
-//                ->update([
-//                    'email' => 'sdad@sdad.sad123',
-//                ]);
-//        } catch (Throwable $e) {
-//            $this->assertInstanceOf(PDOException::class, $e);
-//
-//            // do something, but don't re-throw
-//            // laravel transaction level = 2, mysql transaction level = 1
-//            $caughtException = $e;
-//        }
-//
-//        $this->assertNotNull($caughtException);
-//
-//        // laravel transaction level = 1, mysql transaction level = 0
-//        $secondConnection->rollBack();
-//
-//        // laravel transaction level = 0, mysql transaction level = -1 ?
-//        $secondConnection->rollBack();
-
-        // Must be 1 as DBMS has already decreased the level, but it is in fact 2
-//      $this->assertSame(1, $secondConnection->transactionLevel());
-
-//      $secondConnection->beginTransaction();
-//      $secondConnection->beginTransaction();
-//      $secondConnection->rollBack();
-//      $secondConnection->rollBack();
-        // We now rollback.
-//      $secondConnection->rollBack();
-
-        // J
-//      throw new RuntimeException();
-//    });
     }
 }
